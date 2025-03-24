8295511b31f51b479fe597180aae7e33ba241968
Your Name <your_email@example.com>
2025-03-24 18:43:39 +0530

    update

--- Frontend/src/Component/Home.jsx
+++ Frontend/src/Component/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-
+// import Footer from './Footer';
+import Banner from './Banner';
+import CourseCards from './CourseCard';
+import Something from './Something';
+import Statistics from './Statistics';
+import TrainerSection from './TrainerSection';
+import Testimonials from './TestimonialsData';
 
 function Home() {
   return (
     <>
-      <h1>Hello I'm Home Page</h1>
+      <Banner />
+      <CourseCards />
+      {/* <Footer /> */}
     </>
   );
 }
