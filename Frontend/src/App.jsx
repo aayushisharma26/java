3c3f1b1593651c430af9a9e4a8785b78671fb65d
Your Name <your_email@example.com>
2025-03-24 23:56:17 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
+import './assignment_section/app.css';
+import './About/About.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './About/About.jsx';
-import Home from './Component/Home.jsx';
-<<<<<<< HEAD
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { RiMailCheckFill, RiArrowRightLine } from 'react-icons/ri';
-import Footer from './Component/Footer.jsx'
-=======
->>>>>>> 94f393e (Working on Navbar)
-import Navbar from './Component/Navbar.jsx';
-
+import About from './About/About';
+import Home from './Component/Home';
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
+import CoursesFilter from './courses/CoursesFilter';
+import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+import Layout from './assignment_section/layout';
+import EnrollLayout from './EnrollLayout';
 
 const App = () => {
   return (
@@ -17,13 +18,16 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/courses" element={<CoursesFilter />} />
         <Route path="/about" element={<About />} />
+        <Route path="/layout" element={<Layout />} />
+        <Route path="/lay" element={<EnrollLayout />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
 
-
 export default App;
-
