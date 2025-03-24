a74c7b7f8c1c167c37dc43d98c4708b4c868bba1
Your Name <your_email@example.com>
2025-03-24 16:33:17 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react'
+// import About from './About/About'
 
-function App() {
-  const [count, setCount] = useState(0)
+// const App = () => {
+//   return (
+//     <div>
+//       <About/>
+//       {/* <Home/> */}
+//     </div>
+//   )
+// }
 
+// export default App
+
+
+
+
+
+
+import './App.css';
+// import './assignment_section/app.css';
+// import './About/About.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './About/About.jsx';
+import Home from './Component/Home.jsx';
+// import Login from './Login/Login';
+// import SignUp from './SignUp/SignUp';
+// import CoursesFilter from './courses/CoursesFilter';
+import Navbar from './Component/Navbar.jsx';
+// import Footer from './Component/Footer';
+// import Layout from './assignment_section/layout';
+// import EnrollLayout from './EnrollLayout';
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/signup" element={<SignUp />} /> */}
+        {/* <Route path="/courses" element={<CoursesFilter />} /> */}
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/layout" element={<Layout />} /> */}
+        {/* <Route path="/lay" element={<EnrollLayout />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  );
+};
 
-export default App
+export default App;
