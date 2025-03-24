2fa59f5d437d4ea539223818e77e840332accba2
Your Name <your_email@example.com>
2025-03-24 23:49:22 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './About/About.jsx';
 import Home from './Component/Home.jsx';
+<<<<<<< HEAD
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { RiMailCheckFill, RiArrowRightLine } from 'react-icons/ri';
 import Footer from './Component/Footer.jsx'
+=======
+>>>>>>> 94f393e (Working on Navbar)
 import Navbar from './Component/Navbar.jsx';
 
 
