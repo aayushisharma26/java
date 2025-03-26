b46c4f7d04e820097a9e4983f7402a9d8bb4a77b
Aayushi Sharma <aayushisharma20@navgurukul.org>
2025-03-27 00:00:15 +0530

    Update Login Page

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -45,12 +45,21 @@ export const loginUser = async (req, res) => {
 
     const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    res.json({ token, role: user.role });
+    res.json({ 
+      token, 
+      user: { 
+        id: user._id, 
+        name: user.name, 
+        email: user.email, 
+        role: user.role 
+      } 
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 };
+
 
 export const logoutUser = (req, res) => {
   res.status(200).json({ message: 'Logged out successfully' });
